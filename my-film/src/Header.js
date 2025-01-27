3901d79457e41c1ba21c7a7f4a97daad27a6a3a7
fred <frederic.cassagnou@ensta-paris.fr>
2025-01-27 11:14:03 +0100

    conne front

--- my-film/src/Header.js
+++ my-film/src/Header.js
@@ -8,7 +8,7 @@ import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import Login from "./components/user/Login";
 
-export default function Header() {
+export default function Header({ setIsLoggedIn, isLoggedIn, handleLogout }) {
     const [open, setOpen] = useState(false);
 
     const handleOpen = () => setOpen(true);
@@ -46,7 +46,7 @@ export default function Header() {
 
                 <Typography
                     variant="h6"
-                    onClick={handleOpen}
+                    onClick={isLoggedIn ? handleLogout : handleOpen}
                     sx={{
                         textDecoration: "none",
                         color: "inherit",
@@ -54,12 +54,17 @@ export default function Header() {
                         cursor: "pointer",
                     }}
                 >
-                    Connexion
+                    {isLoggedIn ? "Déconnexion" : "Connexion"}
                 </Typography>
 
                 <Dialog open={open} onClose={handleClose}>
                     <DialogContent>
-                        <Login />
+                        <Login
+                            setAuth={(data) => {
+                                setIsLoggedIn(true);
+                                handleClose();
+                            }}
+                        />
                     </DialogContent>
                 </Dialog>
             </Toolbar>
